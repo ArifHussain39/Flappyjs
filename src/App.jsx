c693b2b9d308b7eb1426b35350234866341c0a6c
Arif Hussain <arif3337179@gmail.com>
2024-03-23 23:01:12 +0500

    Class revision

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  let products = [
+    { id: 1, name: "product 1" },
+    { id: 2, name: "product 2" },
+    { id: 3, name: "product 3" },
+    { id: 4, name: "product 4" },
+    { id: 5, name: "product 5" },
+  ];
+  
 
   return (
     <>
-  
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    
+
+      <ul>
+        {
+         products?.map(product => <li key={product.id} > {product.name} </li>)
+
+        }
+      </ul>
+
+      {
+        products ? <h1>Products</h1> : <h1>No Products</h1>
+      }
+
+
     </>
   )
 }
