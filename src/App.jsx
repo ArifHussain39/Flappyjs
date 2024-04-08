de5290a2bc5df57cbcfd76804e9b3b55fdd80e0d
Arif Hussain <arif3337179@gmail.com>
2024-04-09 01:31:40 +0500

    UI Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,68 @@
-import * as React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+const WALL_HEIGHT = 600;
+const WALL_WIDTH = 400;
+const BIRD_HEIGHT = 50;
+const BIRD_WIDTH = 50;
+const GRAVITY = 5;
 
-export default function CircularDeterminate() {
+const App = () => {
+  const [birdPosition, setBirdPosition] = useState(300);
+
+  useEffect(() => {
+    let birdval;
+    if (birdPosition < WALL_HEIGHT - BIRD_HEIGHT) {
+      birdval = setInterval(() => {
+        setBirdPosition((prev) => prev + GRAVITY);
+      }, 24);
+    }
+    return () => clearInterval(birdval);
+
+  });
+
+  const handler = () => {
+    setBirdPosition((prev) => prev - 50);
+  }
 
   return (
-    <div className="App">
-      <div className="App-header">
-        <h2>My npm package</h2>
-      </div>
-      <div className="App-body">
-        <div className="progress-bar">
-          <div className="progress-bar-fill"></div>
-        </div>
-      </div>
-    </div>
+    <Home onClick={handler}>
+      <Background height={WALL_HEIGHT} width={WALL_WIDTH}>
+        <Bird
+          height={BIRD_HEIGHT}
+          width={BIRD_WIDTH}
+          top={birdPosition}
+          left={100}
+        />
+      </Background>
+    </Home>
   );
 }
+
+export default App;
+
+const Home = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Background = styled.div`
+  background-image: url('https://i.pinimg.com/736x/72/5e/5d/725e5dc00ba49c240cd489e7b87e0496.jpg');
+  background-repeat: no-repeat;
+  background-size: ${(props) => props.width}px ${(props) => props.height}px;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  border: 1px solid black;
+  position: relative;
+`;
+
+const Bird = styled.div`
+  background-image: url('https://img.itch.zone/aW1nLzE0NDQ5NzY4LnBuZw==/315x250%23c/TVnPC7.png');
+  background-repeat: no-repeat;
+  background-size: ${(props) => props.width}px ${(props) => props.height}px;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  position: absolute;
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left}px;
+`;
